2fa64ec7e17e1a03a0922dda6c591868c4247452
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-15 22:29:17 +0600

    new column added #2

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,53 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.white,
           body: SafeArea(
             child: Column(
-              children: const [
-                Center(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontFamily: 'Rubik Medium',
-                        color: Color(0xff2D3142)),
-                  ),
+              children: [
+                const SizedBox(height: 50,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:  [
+                    const Image(
+                      height: 50,
+                      width: 50,
+                      image: AssetImage('images/logo.png'),
+                    ),
+                    const SizedBox(width: 10,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Maintenance',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontFamily: 'Rubik Medium',
+                              color: Color(0xff2D3142)),
+                        ),
+                        Text(
+                          'Box',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontFamily: 'Rubik Medium',
+                              color: Color(0xffF9703B)),
+                        ),
+                      ],
+                    )
+
+                  ],
                 ),
-                SizedBox(height: 14,),
-                Center(
+                const Center(
+                  child: Text(
+                  'Login',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Rubik Medium',
+                      color: Color(0xff2D3142)),
+                ),
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                const Center(
                   child: Text(
                     'This is Basic Login Page,\n we have to login here successfully',
                     textAlign: TextAlign.center,
@@ -36,7 +71,10 @@ class MyApp extends StatelessWidget {
                         fontFamily: 'Rubik Regular',
                         color: Color(0xff4C5980)),
                   ),
-                )
+                ),
+
+
+
               ],
             ),
           ),
